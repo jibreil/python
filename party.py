3857c1a2bdc1e525b355a96898431e1c1ce2e284
Jibreil <jibreilhoneine@gmail.com>
2018-09-04 20:25:52 +1000

    update

--- party.py
+++ party.py
@@ -4,67 +4,72 @@
 from time import sleep
 
 sleep(1)
-print("\n ~~ Welcome to the digital invitation system for my Python Party ~~")
+print("\n~~ Welcome to the digital invitation system for my Python Party ~~")
 sleep(1)
 
-print("\n ~ Here's some information about the party")
+print("\n~ Here's some information about the party")
 sleep(1)
 
-print("\n\t ~ Date: Janurary 17th")
+print("\n\t~ Date: Janurary 17th")
 sleep(1)
 
-print("\n\t ~ Time: 6:30 PM")
+print("\n\t~ Time: 6:30 PM")
 sleep(1)
 
-print("\n\t ~ Location: 123 Sesame Street")
+print("\n\t~ Location: 123 Sesame Street")
 sleep(1)
 
-name = input("\n ~ What is your name? ")
+name = str(input("\n~ What is your name?"))
 sleep(0.5)
 
-print("\n\t ~ Thanks for answering " + name + ".")
+print("\n\t~ Thanks for answering " + name + ".")
 sleep(1)
 
-bring = input("\n ~ Will you be bringing anything? (Yes/No): ")
+bring = str(
+    input("\n~ Will you be bringing anything? (Please answer yes or no):"))
 bring = bring.casefold()
 sleep(0.5)
 
 if bring == "yes":
-    bringing = input("\n\t ~ What will you be bringing? ")
+    bringing = str(input("\n\t~ What will you be bringing?"))
     bringing = bringing.casefold()
     sleep(0.5)
-    print("\n\t ~ Thank you " + name + ".")
+    print("\n\t~ Thank you " + name + ".")
     sleep(1)
 if bring == "no":
-    print("\n\t ~ That's okay, " + name + ".")
+    print("\n\t~ That's okay, " + name + ".")
+    bringing = str("nothing")
     sleep(1)
 
-print("\n ~ Here is a summary of the party for you " + name + ".")
+print("\n~ Here is a summary of the party for you " + name + ".")
 sleep(1)
 
-print("\n\t ~ Date: Janurary 17th")
+print("\n\t~ Date: Janurary 17th")
 sleep(1)
 
-print("\n\t ~ Time: 6:30 PM.")
+print("\n\t~ Time: 6:30 PM.")
 sleep(1)
 
-print("\n\t ~ Location: 123 Sesame Street")
+print("\n\t~ Location: 123 Sesame Street")
 sleep(1)
 
-print("\n\t ~ You said you will be bringing " + bringing + ".")
+print("\n\t~ You said you will be bringing " + bringing + ".")
 sleep(1)
 
-confirmation = input("\n ~ Is this all correct? (Yes/No): ")
-confirmation = confimation.casefold()
+confirmation = str(
+    input("\n~ Is this all correct? (Please answer yes or no):"))
+confirmation = confirmation.casefold()
 sleep(0.5)
 
 if confirmation == "yes":
     sleep(1)
-    print("\n\t ~ Great! Can't wait to see you there " + name + ".")
+    print("\n\t~ Great! Can't wait to see you there " + name + ".")
     sleep(1)
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     quit()
 if confirmation == "no":
     sleep(1)
-    print("\n\t ~ That's too bad," + name + ".")
+    print("\n\t~ That's too bad," + name + ".")
     sleep(1)
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     quit()
